5a7a246096575f22f3eec5f00fad2341c5e22978
luoluona <luoluona@ust.hk>
2022-05-16 16:44:43 +0800

    lab2 finished

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+        while(ptr.rest!=null){
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +96,16 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList newList = new IntList(A.first, null);
+        IntList ptr = newList;
+        IntList p = A;
+        while(p.rest!=null){
+            p = p.rest;
+            ptr.rest = new IntList(p.first, null);
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return newList;
     }
 
 
