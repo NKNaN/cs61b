2b851741762da52abbe675fbc9bd20e16f82fdde
luoluona <luoluona@ust.hk>
2022-05-22 16:18:17 +0800

    lab3 v1

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -70,4 +70,22 @@ public class IntListTest {
       * to add a main method. See ArithmeticTest.java for an
       * example. */
 
+    /**
+     * Returns the reverse of the given IntList.
+     * This method is destructive. If given null
+     * as an input, returns null.
+     */
+    @Test
+    public void testReverse() {
+        IntList a = IntList.of(1, 2, 3, 4, 5);
+        IntList exp = IntList.of(5, 4, 3, 2, 1);
+        IntList actual = IntList.reverse(a);
+        assertEquals(exp, actual);
+
+        assertNotEquals(actual, a);
+
+        IntList b = null;
+        IntList actual2 = IntList.reverse(b);
+        assertEquals(null, actual2);
+    }
 }
