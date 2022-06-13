eb695cbc2bb21a2f141e5dd9cb13fd932a715c95
luoluona <luoluona@ust.hk>
2022-06-13 20:42:56 +0800

    hw2 v1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,132 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int[][] grids; // 0: blocked; 1: open
+    private WeightedQuickUnionUF grids1DTop;
+    private WeightedQuickUnionUF grids1DBottom;
+    private int numberOfOpenSites;
+    private int top; // index in UF
+    private int bottom; // index in UF
 
+    private boolean percolate = false;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N should be greater than 0.");
+        }
+        grids = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grids[i][j] = 0;
+            }
+        }
+        grids1DTop = new WeightedQuickUnionUF(N * N + 1);
+        grids1DBottom = new WeightedQuickUnionUF(N * N + 1);
+        top = N * N;
+        bottom = N * N;
+        numberOfOpenSites = 0;
+    }
+
+    public void open(int row, int col) {
+        if (row < 0 || row >= grids.length || col < 0 || col >= grids.length) {
+            throw new IndexOutOfBoundsException("Index range should be [0, N-1].");
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        grids[row][col] = 1;
+        numberOfOpenSites++;
+        connectSurroundingOpenSite(row, col);
+        if (row == 0) {
+            grids1DTop.union(top, xyTo1D(row, col));
+        } else if (row == grids.length - 1) {
+            grids1DBottom.union(bottom, xyTo1D(row, col));
+        }
+        if (isFull(row, col) && grids1DBottom.connected(bottom, xyTo1D(row, col))) {
+            percolate = true;
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= grids.length || col < 0 || col >= grids.length) {
+            throw new IndexOutOfBoundsException("Index range should be [0, N-1].");
+        }
+        return grids[row][col] == 1;
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= grids.length || col < 0 || col >= grids.length) {
+            throw new IndexOutOfBoundsException("Index range should be [0, N-1].");
+        }
+        return grids1DTop.connected(top, xyTo1D(row, col));
+    }
+
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+
+    public boolean percolates() {
+        return percolate;
+    }
+
+    private int xyTo1D(int x, int y) {
+        int N = grids.length;
+        return N * x + y;
+    }
+
+    private void connectSurroundingOpenSite(int row, int col) {
+        int N = grids.length;
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if ((i == -1 || i == 1) && (j == -1 || j == 1)) {
+                    continue;
+                }
+                int x = row + i;
+                int y = col + j;
+                if (x >= 0 && x < N && y >= 0 && y < N) {
+                    if (isOpen(x, y)) {
+                        grids1DTop.union(xyTo1D(x, y), xyTo1D(row, col));
+                    }
+                }
+            }
+        }
+    }
+/*
+    public static void main(String[] args) {
+        Percolation p = new Percolation(4);
+        System.out.println(p.numberOfOpenSites()); // 0
+        System.out.println(p.isFull(1,3)); // f
+        System.out.println(p.isOpen(1,3)); // f
+        p.open(1,3);
+        System.out.println(p.isFull(1,3)); // f
+        System.out.println(p.isOpen(1,3)); // t
+        System.out.println("--------------------\n");
+        p.open(2,2);
+        System.out.println(p.isFull(2,2)); // f
+        System.out.println(p.isOpen(2,2)); // t
+        System.out.println(p.isFull(1,2)); // f
+        System.out.println(p.isOpen(1,2)); // f
+        System.out.println("--------------------\n");
+        p.open(0,3);
+        System.out.println(p.isFull(0,3)); // t
+        System.out.println(p.isOpen(0,3)); // t
+        System.out.println(p.isFull(1,3)); // t
+        System.out.println(p.isOpen(1,3)); // t
+        System.out.println(p.isFull(2,2)); // f
+        System.out.println(p.isOpen(2,2)); // t
+        System.out.println(p.numberOfOpenSites()); // 3
+        System.out.println("--------------------\n");
+        p.open(1,2);
+        System.out.println(p.isFull(1,2)); // t
+        System.out.println(p.isOpen(1,2)); // t
+        System.out.println(p.isFull(2,2)); // t
+        System.out.println(p.isOpen(2,2)); // t
+        System.out.println(p.numberOfOpenSites()); // 4
+        System.out.println(p.percolates()); // f
+        System.out.println("--------------------\n");
+        p.open(3,2);
+        System.out.println(p.numberOfOpenSites()); // 5
+        System.out.println(p.percolates()); // t
+    }
+*/
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,59 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
 
+    private double[] fracs;
+
+    private int[] oneDtoXy(int x, int N) {
+        int[] ret = new int[2];
+        ret[0] = x / N;
+        ret[1] = x % N;
+        return ret;
+    }
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Arguments should be greater than 0");
+        }
+        Percolation p = null;
+        fracs = new double[T];
+        for (int i = 0; i < T; i++) {
+            p = pf.make(N);
+            while (!p.percolates()) {
+                int r = StdRandom.uniform(N * N);
+                int[] point = oneDtoXy(r, N);
+                p.open(point[0], point[1]);
+            }
+            fracs[i] = (double) p.numberOfOpenSites() / (N * N);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(fracs);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(fracs);
+    }
+
+    public double confidenceLow() {
+        int t = fracs.length;
+        return mean() - 1.96 * stddev() / Math.sqrt(t);
+    }
+
+    public double confidenceHigh() {
+        int t = fracs.length;
+        return mean() + 1.96 * stddev() / Math.sqrt(t);
+    }
+
+/*    public static void main(String[] arg) {
+        PercolationStats p = new PercolationStats(20, 50, new PercolationFactory());
+        System.out.println(p.mean());
+        System.out.println(p.stddev());
+        System.out.println(p.confidenceLow());
+        System.out.println(p.confidenceHigh());
+    }*/
 }
